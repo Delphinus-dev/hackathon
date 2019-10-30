c6698f6ba355b5f88005a585d50face78f802ecc
corinne <cdallongeville22@gmail.com>
2019-10-30 20:17:21 +0100

    accueil avec css

--- public/index.php
+++ public/index.php
@@ -1,10 +1,122 @@
-<?php include 'inc/header.php';?>
-    <main role="main" class="container">
-        <div class="jumbotron">
-            <h1>Bienvenue</h1>
-            <p class="lead">Exemple de 1ère page.</p>
-            <a class="btn btn-lg btn-primary" href="/" role="button">Lire la suite &raquo;</a>
-        </div>
-    </main>
+<!doctype html>
+<html lang="en">
 
-<?php include 'inc/footer.php';?>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Laurent Touret">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <link rel="stylesheet" type="text/css" href="css/app.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Prompt&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Prompt&display=swap" rel="stylesheet">
+
+    <title>Horror Quizz</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="css/app.css" rel="stylesheet">
+</head>
+
+<body class="container">
+    <section class="home">
+        <div>
+        <button type="button" class="btn btn-warning">JOUER</button>
+        </div>
+        <div>
+        <img src="images/home.png" alt>
+        </div>
+    </section>
+
+    <section  id="accordion">
+        <div class="card">
+            <div class="card-header" id="headingOne">
+                <h5 class="mb-0">
+                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Les instructions, c'est ici !
+                    </button>
+                </h5>
+            </div>
+
+            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                <div class="card-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="halloffame">
+        <div id="podium-box" class="row" style="height: 300px">
+            <div class="col-md-4 step-container m-0 p-0">
+                <div>
+                    Text 2
+                </div>
+                <div id="second-step" class="bg-blue step centerBoth podium-number">
+                    2
+                </div>
+            </div>
+            <div class="col-md-4 step-container m-0 p-0">
+                <div>
+                    Text 1
+                </div>
+                <div id="first-step" class="bg-blue step centerBoth podium-number">
+                    1
+                </div>
+            </div>
+            <div class="col-md-4 step-container m-0 p-0">
+                <div>
+                    Text 3
+                </div>
+                <div id="third-step" class="bg-blue step centerBoth podium-number">
+                    3
+                </div>
+            </div>
+        </div>
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">Classement</th>
+                <th scope="col">Monstre</th>
+                <th scope="col">Nickname</th>
+                <th scope="col">Points</th>
+                <th scope="col">Date</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">4</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">5</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            </tr>
+            <tr>
+                <th scope="row">6</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            </tr>
+            </tbody>
+            </table>
+
+    </section>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
