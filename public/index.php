95a849a2a3049306026d62e09d1e8bac404fd4c9
monsieur486 <monsieur486@gmail.com>
2019-10-30 12:18:10 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php include 'inc/header.php';?>
+    <main role="main" class="container">
+        <div class="jumbotron">
+            <h1>Bienvenue</h1>
+            <p class="lead">Exemple de 1ère page.</p>
+            <a class="btn btn-lg btn-primary" href="/" role="button">Lire la suite &raquo;</a>
+        </div>
+    </main>
+
+<?php include 'inc/footer.php';?>
