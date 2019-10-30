1be6f0d9db36d0193036500d35c5340dca193fe1
Edouard <edouard.neveu67@gmail.com>
2019-10-30 23:48:04 +0100

    blabla

--- public/jeu.php
+++ public/jeu.php
@@ -18,9 +18,9 @@
     <link href="css/app.css" rel="stylesheet">
 
     <!-- CDN Javascript bootstrap -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-            crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 
 </head>
@@ -49,7 +49,7 @@
             </div>
             <div class="row visuSeringue">
                 <div class="progress" >
-                    <div class="progress-bar bg-danger" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                    <div class="progress-bar progress-bar2 bg-danger" id="progressbar" data-top="50" data-height="50" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
                     </div>
                 </div>
                 <div class=""></div>
@@ -59,7 +59,7 @@
     <div class="row gestionIndices">
         <div class="col-6 indRestants">
             <p> X indices Restants</p>
-            <button type="button" class="btn btn-primary">indice pour Y litres ?</button>
+            <button type="button" id="bouton-ind-restants" class="btn btn-primary">indice pour Y litres ?</button>
         </div>
         <div class="col-6 bonusRestants">
             <p> X super-bonus</p>
@@ -100,3 +100,40 @@
 
 
 </main>
+
+<script>
+
+    // script jquery (+ simple que js --- pas besoin de GET, etc...)
+    $("body").on("click", "#bouton-ind-restants", function () {
+        $("#progressbar").css("top", function() {
+            niveau_top = parseInt($("#progressbar").attr("data-top"));
+            // niveau_height = parseInt($("#progressbar").attr("data-height"));
+            calcul =  (niveau_top * 1.05) + '%';
+            $("#progressbar").attr("data-top", calcul);
+            return calcul;
+        });
+        $("#progressbar").css("height", function() {
+            niveau_height = parseInt($("#progressbar").attr("data-height"));
+            calcul =  (niveau_height * 0.95) + '%';
+            $("#progressbar").attr("data-height", calcul);
+            return calcul;
+        });
+
+    });
+        /*iveau_top = parseInt($("#progressbar").attr("data-top"));
+        niveau_height = parseInt($("#progressbar").attr("data-height"));
+        niveau_top -= 5;
+        niveau_height += 5;
+        nt = "" + niveau_top + "%";
+        nh = "" + niveau_height + "%";
+        // $("#progressbar").removeClass("progress-bar2");
+        $("#progressbar").css({top:"70%", height:"30%"});*/
+
+
+   /* $("#bouton-ind-restants").click(function ()
+    {
+        var niveau = $("#progressbar").attr("aria-valuenow");
+        niveau -= 5;
+        $("#progressbar").attr("aria-valuenow", niveau);
+    });*/
+</script>
