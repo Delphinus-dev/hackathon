fcd198033445e63a8a5efa9a895e194a086c47b4
monsieur486 <monsieur486@gmail.com>
2019-10-31 11:23:13 +0100

    Règles du jeux

--- public/achatIndice.php
+++ public/achatIndice.php
@@ -4,6 +4,10 @@ include '../src/functions.php';
 if ($_SESSION["indicesRestants"]>=0) {
     $_SESSION["indicesRestants"]-=1;
     $_SESSION["vie"]-=$prixIndice;
+    if ($_SESSION["vie"]<=0){
+        header('Location: gameover.php');
+        exit;
+    }
 }
 
 header('Location: jeu.php');
